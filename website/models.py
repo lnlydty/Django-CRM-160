607eaef4bae242fdb6408fcaca30b2f593b1180d
Alliah Mae <adhisuan@up.edu.ph>
2024-10-03 12:05:18 +0800

    date failed

--- website/models.py
+++ website/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Record(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
 	email = models.CharField(max_length=100)
 	phone = models.CharField(max_length=20)
-	last_name = models.CharField(max_length=50)
 	address = models.CharField(max_length=100)
 	city = models.CharField(max_length=50)
 	state = models.CharField(max_length=50) 
@@ -16,5 +16,4 @@ class Record(models.Model):
 	def __str__(self):
 		return(f"{self.first_name} {self.last_name}")
 		
-
 
